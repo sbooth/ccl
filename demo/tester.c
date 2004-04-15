8e66cc7cbe6e30bc93364b05dd60adc936da0e05
Stephen F. Booth <me@sbooth.org>
2004-04-15 17:54:34 +0000

    Fixed #includes

--- demo/tester.c
+++ demo/tester.c
@@ -1,38 +1,38 @@
 /*
- *  $Id: tester.c,v 1.2 2004-04-14 15:32:46 sbooth Exp $
+ *  $Id: tester.c,v 1.3 2004-04-15 17:54:34 sbooth Exp $
  *
  *  Copyright (C) 2004 Stephen F. Booth
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- *  This library is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 #include <stdio.h>
 
-#include "ccl.h"
+#include "ccl/ccl.h"
 
 void
 usage(const char *prog_name)
 {
-  printf("usage: %s config_file_name\n", prog_name);
+  printf("usage: %s config_file\n", prog_name);
 }
 
 int
 main(int argc,
      char **argv)
 {
-  struct ccl_t config;
-  const struct ccl_pair_t *iter;
+  struct ccl_t 			config;
+  const struct ccl_pair_t 	*iter;
 
   if(argc == 1) {
     usage(argv[0]);
